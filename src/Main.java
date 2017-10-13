d0c93095a2b8a5a6b7d40d72a5bf8cf94d04b8c3
BuildTools <leskauskas25@gmail.com>
2017-10-13 17:36:18 +0300

    - updated

--- src/Main.java
+++ src/Main.java
@@ -25,13 +25,53 @@ public class Main extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JFrame firstFrame = new JFrame("First");
                 JPanel firstPanel = new JPanel();
-                firstPanel.setLayout(new FlowLayout());
-                JButton someButton = new JButton("it gonna do sumtin");
-                firstFrame.setSize(200, 150);
-                firstPanel.add(someButton);
+
+               // firstPanel.setLayout(new GridLayout(3, 2));
+                firstPanel.setLayout(null);
+
+                JLabel nameLabel = new JLabel("First name:");
+                JLabel lastNameLabel = new JLabel("Last name:");
+                JTextField nameField = new JTextField(20);
+                JTextField lastNameField = new JTextField(20);
+                JLabel ageLabel = new JLabel("Age:");
+                JTextField ageField = new JTextField(20);
+                JButton button = new JButton("OK");
+
+                firstFrame.setSize(300, 180);
+
+                nameLabel.setBounds(10, 20, 80, 25);
+                firstPanel.add(nameLabel);
+                nameField.setBounds(90,20,165,25);
+                firstPanel.add(nameField);
+
+                lastNameLabel.setBounds(10, 50, 80, 25);
+                firstPanel.add(lastNameLabel);
+                lastNameField.setBounds(90, 50, 165, 25);
+                firstPanel.add(lastNameField);
+
+                ageLabel.setBounds(10, 80, 80, 25);
+                firstPanel.add(ageLabel);
+                ageField.setBounds(90, 80, 50, 25);
+                firstPanel.add(ageField);
+
+                button.setBounds(174, 110, 80, 25);
+                firstPanel.add(button);
                 firstFrame.getContentPane().add(firstPanel);
 
                 firstFrame.setVisible(true);
+
+                //OK button listener
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Integer age = Integer.parseInt(ageField.getText());
+
+                        if (age < 21) {
+                            //unchecked exception
+                            throw new ArithmeticException("You must be at least 21 years old");
+                        }
+                    }
+                });
 
             }
         });
